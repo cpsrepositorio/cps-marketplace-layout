428f30d598ece3e25d5f1ee9cf8d68e817466444
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2022-05-31 17:35:06 -0300

    chore: deploy

--- assets/AboutView.9888e3a4.js
+++
@@ -1,1 +0,0 @@
-import{u as e,o as t,c as o}from"./app.8ef7b6da.js";const s={class:"text-xl font-semibold"},n={name:"AboutView",setup(a){return e({title:"Sobre - CPS Marketplace"}),(c,r)=>(t(),o("h2",s,"Sobre"))}};export{n as default};
