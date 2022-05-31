2c475616858cb52f4b6fc0332573da1cfa1497a1
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2022-05-31 16:22:23 -0300

    chore: deploy

---
+++ assets/FavoritesView.c27dc45d.js
@@ -0,0 +1,1 @@
+import{u as e,o as t,c as o}from"./app.6bc78ef0.js";const s={class:"text-xl font-semibold"},l={name:"FavoritesView",setup(a){return e({title:"Favoritos - CPS Marketplace"}),(r,c)=>(t(),o("h2",s,"Favoritos"))}};export{l as default};
